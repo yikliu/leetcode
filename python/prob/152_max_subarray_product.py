127863d1c4db5713117724a1a7d66b1e3723a6e0
yikliu <yikliu@yiklius-imac.lan>
2022-01-04 00:17:08 -0800

    update

---
+++ python/prob/152_max_subarray_product.py
@@ -0,0 +1,23 @@
+'''
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+'''
+
+def maxProduct(nums: List[int])-> int:
+    max_i = [nums[0]]
+    min_i = [nums[0]]
+    res = nums[0]
+    for i in range(1, len(nums)):
+        max_i.append(max(max_i[i-1] * nums[i], min_i[i-1] * nums[i], nums[i]))
+        min_i.append(min(min_i[i-1] * nums[i], max_i[i-1] * nums[i], nums[i]))
+        res = max(res, max_i[i])
+    return res
---
+++ python/prob/16_3_sum_close.py
@@ -0,0 +1,27 @@
+'''
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+For example, given array S = {-1 2 1 -4}, and target = 1.
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+'''
+
+def threeSumClose(nums: List[int], target:int) -> int:
+    n = len(nums)
+    nums = sorted(nums)
+    res = nums[0] + nums[1] + nums[2]
+    
+    for i in range(n - 2):
+        start = i + 1
+        end = n - 1
+        while start < end:
+            temp = nums[i] + nums[start] + nums[end]
+            if abs(target - temp) < abs(target - res):
+                res = temp
+            if res == target:
+                return res
+            if temp < target:
+                start += 1
+            else:
+                end -= 1
+    return res
